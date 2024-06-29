59c112226c4f7a2da10c6100d6de47cc38050fcd
SaeedSafi1999 <safi.saeed1999@gmail.com>
2024-06-29 15:48:43 +0330

    fix

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/IdentityManager', AuthRoutes);
 app.use('/api/Role',RoleRoutes)
 
+
 // Swagger setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
